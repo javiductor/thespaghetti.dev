9d632153c68d312dd9ce4d2f2a2af019f4a03f5b
Javier Ductor <javi@theatomlab.co.uk>
2024-11-23 11:57:50 +0200

    Changed file path for images - home page

--- src/components/pages/home/components/featured-projects.jsx
+++ src/components/pages/home/components/featured-projects.jsx
@@ -5,28 +5,28 @@ import styles from "./featured-projects.module.css";
 const FeaturedProjects = () => {
   const projects = [
     {
-      thumbImageSrc: "/public/assets/Portfolio/TTHC.webp",
+      thumbImageSrc: "/assets/Portfolio/TTHC.webp",
       altDescription: "Henley Spanish Centre Portfolio",
       category: ["CSS", "HTML", "React JS"],
       heading: "Henley Spanish Centre",
       link: "/work/henley-spanish-centre",
     },
     {
-      thumbImageSrc: "/src/assets/portfolio/TTHC.webp",
+      thumbImageSrc: "/assets/Portfolio/TTHC.webp",
       altDescription: "The Spaghetti Dev Portfolio",
       category: ["CSS", "HTML", "React JS"],
       heading: "Thespaghetti.dev",
       link: "/work/spaghetti",
     },
     {
-      thumbImageSrc: "/src/assets/portfolio/mrp.webp",
+      thumbImageSrc: "/assets/Portfolio/mrp.webp",
       altDescription: "mrp Portfolio",
       category: ["Web Design", "SEO", "Branding"],
       heading: "mrp",
       link: "/work/mrp",
     },
     {
-      thumbImageSrc: "/src/assets/portfolio/TTHC.webp",
+      thumbImageSrc: "/assets/Portfolio/TTHC.webp",
       altDescription: "Torres Heights Portfolio",
       category: ["Web Design"],
       heading: "Torres Heights",
