dbd3cc85089d76600d57518ba53b7bbed740b5ad
Javier Ductor <javi@theatomlab.co.uk>
2024-11-25 17:44:10 +0200

    New footer and more mobile optmisation

--- src/components/shared/footer.jsx
+++ src/components/shared/footer.jsx
@@ -2,28 +2,34 @@ import React from "react";
 import styles from "./footer.module.css";
 
 const Footer = () => {
+  const handleEmailClick = () => {
+    window.location.href = "mailto:javi@theatomlab.co.uk";
+  };
+
   return (
-    <>
-      <div className={styles.footerContainer}>
-        <div className={styles.footerInfo}>
-          <p className={styles.footerLogo}>thespaghetti.dev</p>
-          <div className={styles.footerLinks}>
-            <div className={styles.footerList}>
-              <p>Front-end Developer</p>
-              <p>Branding & Vision</p>
-              <p>Web Design</p>
-            </div>
-            <div className={styles.footerMenu}>
-              <a href="/">home</a>
-              <a href="/work">work</a>
-              <a href="/about">about</a>
-              <a href="/blog">blog</a>
-            </div>
+    <div className={styles.footerContainer}>
+      <div className={styles.footerInfo}>
+        <p className={styles.footerLogo}>thespaghetti.dev</p>
+        <div className={styles.footerContactInfo}>
+          <a href="mailto:javi@theatomlab.co.uk">javi@theatomlab.co.uk</a>
+          <a href="tel:07495477305">07495477305</a>
+        </div>
+        <div className={styles.footerLinks}>
+          <div className={styles.footerMenu}>
+            <a href="/">home</a>
+            <a href="/work">work</a>
+            <a href="/about">about</a>
+            <a href="/blog">blog</a>
           </div>
         </div>
-        <div className={styles.footerForm}></div>
       </div>
-    </>
+      <div className={styles.footerRight}>
+        <h3>Looking for a front-end developer?</h3>
+        <button onClick={handleEmailClick} className={styles.btnCTA}>
+          Hire me!
+        </button>
+      </div>
+    </div>
   );
 };
 
