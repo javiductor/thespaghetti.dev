6739db71a25719fcd4245a57c4aad0e5481ff9fe
Javier Ductor <javi@theatomlab.co.uk>
2024-12-05 16:13:54 +0200

    bug fix SEOHead.jsx

--- src/components/SeoHead.jsx
+++ src/components/SeoHead.jsx
@@ -4,7 +4,7 @@ import { Helmet } from "react-helmet-async";
 const SEO = ({
   title = "The Spaghetti Dev",
   description = "Follow my journey as a developer through hands-on projects, coding insights, and real-world experiences. Explore my portfolio and learn alongside me as I navigate the tech world.",
-  ogImage = "https://thespaghetti.dev/assets/SEO/seo-home.webp",
+  ogImage = "/assets/SEO/seo-home.webp",
   canonicalUrl,
   noindex = false,
 }) => {
@@ -17,7 +17,6 @@ const SEO = ({
     <Helmet>
       {/* Primary Meta Tags */}
       <title>{title}</title>
-      <meta name="title" content={title} />
       <meta name="description" content={description} />
 
       {/* Open Graph / Facebook */}
@@ -32,12 +31,11 @@ const SEO = ({
 
       {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:url" content={canonicalUrl || siteUrl} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={fullImageUrl} />
 
-      {/* Canonical URL */}
+      {/* Canonical */}
       {canonicalUrl && <link rel="canonical" href={canonicalUrl} />}
 
       {/* Robots */}
