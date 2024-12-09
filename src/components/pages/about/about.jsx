2ad7e0865ab1a61662dffbd261ee7b48e502868b
Javier Ductor <javi@theatomlab.co.uk>
2024-12-09 18:35:14 +0200

    Changed back to SEH-Head

--- src/components/pages/about/about.jsx
+++ src/components/pages/about/about.jsx
@@ -2,28 +2,17 @@ import React from "react";
 import AboutHeader from "./components/about-header";
 import Divider from "../../shared/divider";
 import AboutInfo from "./components/about-info";
-import { Helmet, HelmetProvider } from "react-helmet-async";
-// import SEO from "../../SeoHead";
+import SEO from "../../SeoHead";
 
 function About() {
   return (
     <>
-      <Helmet>
-        <title>About Me | The Spaghetti Dev</title>
-        <meta
-          name="description"
-          content="A front-end developer and digital designer with nearly 10 years experience in digital marketing and web design."
-        />
-        <meta property="og:title" content="About Me | The Spaghetti Dev" />
-        <meta
-          property="og:description"
-          content="A front-end developer and digital designer with nearly 10 years experience in digital marketing and web design."
-        />
-        <meta
-          property="og:image"
-          content="https://thespaghetti.dev/assets/SEO/seo-about.webp"
-        />
-      </Helmet>
+      <SEO
+        title="About Me | The Spaghetti Dev"
+        description="A front-end developer and digital designer with nearly 10 years experience in digital marketing and web design."
+        ogImage="https://thespaghetti.dev/assets/SEO/seo-about.webp"
+        canonicalUrl="https://thespaghetti.dev/about"
+      />
       <div className="App">
         <AboutHeader />
         <Divider header="About Me" />
--- src/components/pages/blog/blog.jsx
+++ src/components/pages/blog/blog.jsx
@@ -3,7 +3,6 @@ import { createDirectus, rest, readItems } from "@directus/sdk";
 import Card from "../../shared/card";
 import styles from "./blogpage.module.css";
 import SEO from "../../SeoHead";
-import { Helmet, HelmetProvider } from "react-helmet-async";
 
 const directus = createDirectus("https://api.theatomlab.co.uk").with(rest());
 
@@ -36,49 +35,37 @@ const Blog = () => {
 
   return (
     <>
-      <HelmetProvider>
-        <Helmet>
-          <title>My blog | The Spaghetti Dev</title>
-          <meta
-            name="description"
-            content="Take a look at my blog about my journey in web development so far."
-          />
-          <meta property="og:title" content="My blog | The Spaghetti Dev" />
-          <meta
-            property="og:description"
-            content="Take a look at my blog about my journey in web development so far."
-          />
-          <meta
-            property="og:image"
-            content="https://thespaghetti.dev/assets/SEO/seo-about.webp"
-          />
-        </Helmet>
-        <div className={styles.Header}>
-          <h1>My Blog</h1>
+      <SEO
+        title="My blog | The Spaghetti Dev"
+        description="Take a look at my blog about my journey in web development so far."
+        ogImage="https://thespaghetti.dev/assets/SEO/seo-blog.webp"
+        canonicalUrl="https://thespaghetti.dev/blog"
+      />
+      <div className={styles.Header}>
+        <h1>My Blog</h1>
+      </div>
+      <div className={styles.pageContainer}>
+        <div className={styles.blogGrid}>
+          {blogs.map((BlogPost) => (
+            <Card
+              key={BlogPost.id}
+              thumbImageSrc={
+                BlogPost.Featured_Img
+                  ? `https://api.theatomlab.co.uk/assets/${BlogPost.Featured_Img}`
+                  : "/placeholder-image.jpg"
+              }
+              category={
+                Array.isArray(BlogPost.Categories) ? BlogPost.Categories : []
+              }
+              altDescription={BlogPost.Title}
+              heading={BlogPost.Title}
+              link={`/blog/${BlogPost.slug}`}
+              showArrow={false}
+              showHeadingMargin={false}
+            />
+          ))}
         </div>
-        <div className={styles.pageContainer}>
-          <div className={styles.blogGrid}>
-            {blogs.map((BlogPost) => (
-              <Card
-                key={BlogPost.id}
-                thumbImageSrc={
-                  BlogPost.Featured_Img
-                    ? `https://api.theatomlab.co.uk/assets/${BlogPost.Featured_Img}`
-                    : "/placeholder-image.jpg"
-                }
-                category={
-                  Array.isArray(BlogPost.Categories) ? BlogPost.Categories : []
-                }
-                altDescription={BlogPost.Title}
-                heading={BlogPost.Title}
-                link={`/blog/${BlogPost.slug}`}
-                showArrow={false}
-                showHeadingMargin={false}
-              />
-            ))}
-          </div>
-        </div>
-      </HelmetProvider>
+      </div>
     </>
   );
 };
